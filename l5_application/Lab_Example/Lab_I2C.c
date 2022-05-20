19eba4dc188c8b64290c5292901e5716881bdabb
Ruihua Cai <crh0126@gmail.com>
2022-05-20 00:42:01 -0700

    renamed:    l5_application/MP3_lib_Source/R_CTRL.c -> l3_drivers/MP3_lib_Source/R_CTRL.c renamed:    l5_application/MP3_lib_Source/R_LCD.c -> l3_drivers/MP3_lib_Source/R_LCD.c renamed:    l5_application/MP3_lib_Source/R_MP3.c -> l3_drivers/MP3_lib_Source/R_MP3.c renamed:    l5_application/MP3_lib_Source/R_Status.c -> l3_drivers/MP3_lib_Source/R_Status.c renamed:    l5_application/R_CTRL.h -> l3_drivers/R_CTRL.h renamed:    l5_application/R_LCD.h -> l3_drivers/R_LCD.h renamed:    l5_application/R_MP3.h -> l3_drivers/R_MP3.h renamed:    l5_application/R_Status.h -> l3_drivers/R_Status.h deleted:    l5_application/Lab_Example/Lab_GPIO.c deleted:    l5_application/Lab_Example/Lab_I2C.c deleted:    l5_application/Lab_Example/Lab_Midterm.c deleted:    l5_application/Lab_Example/Lab_PWM_ADC.c deleted:    l5_application/Lab_Example/Lab_SPI.c deleted:    l5_application/Lab_Example/Lab_Task.c deleted:    l5_application/Lab_Example/Lab_UART.c deleted:    l5_application/Lab_Example/Lab_Watdog.c deleted:    l5_application/Lab_Example/Lab_inter.c deleted:    l5_application/Rex_Driver_Lib/gpio_isr.c deleted:    l5_application/Rex_Driver_Lib/gpio_isr.h deleted:    l5_application/Rex_Driver_Lib/gpio_lab.c deleted:    l5_application/Rex_Driver_Lib/gpio_lab.h 	deleted:    l5_application/Rex_Driver_Lib/i2c_slave_functions.c 	deleted:    l5_application/Rex_Driver_Lib/i2c_slave_functions.h 	deleted:    l5_application/Rex_Driver_Lib/i2c_slave_init.c 	deleted:    l5_application/Rex_Driver_Lib/i2c_slave_init.h 	deleted:    l5_application/Rex_Driver_Lib/ssp2_lab.c 	deleted:    l5_application/Rex_Driver_Lib/ssp2_lab.h 	deleted:    l5_application/Rex_Driver_Lib/uart_lab.c 	deleted:    l5_application/Rex_Driver_Lib/uart_lab.h

--- l5_application/Lab_Example/Lab_I2C.c
+++
@@ -1,9 +0,0 @@
-#if 0
-
-void main(void) {
-  i2c2__slave_init(0xFE);
-  // sj2_cli__init();
-  vTaskStartScheduler();
-}
-
-#endif
--- l5_application/Rex_Driver_Lib/i2c_slave_functions.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdbool.h>
-#include <stdint.h>
-
-bool i2c_slave_callback__read_memory(uint8_t memory_index, uint8_t *memory);
-
-bool i2c_slave_callback__write_memory(uint8_t memory_index, uint8_t memory_value);
